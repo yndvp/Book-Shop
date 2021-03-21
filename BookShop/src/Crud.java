5c24276f6caf1e15895285d0da29cad2bfad1a77
yndvp <yunna3828@gmail.com>
2021-03-21 19:14:27 +0900

    Final

--- BookShop/src/Crud.java
+++ BookShop/src/Crud.java
@@ -21,6 +21,8 @@ import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Crud {
 
@@ -126,7 +128,7 @@ public class Crud {
 					pst.setString(3, price);
 					pst.executeUpdate();
 		
-					JOptionPane.showMessageDialog(null, "Record Added!");
+					JOptionPane.showMessageDialog(null, "Record Added");
 					table_load();
 					txtname.setText("");
 					txtedition.setText("");
@@ -169,12 +171,28 @@ public class Crud {
 		txtprice.setBounds(207, 151, 201, 26);
 		panel.add(txtprice);
 		
+		// Exit the program
 		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		btnExit.setFont(new Font("Gulim", Font.PLAIN, 13));
 		btnExit.setBounds(217, 222, 114, 60);
 		panel.add(btnExit);
 		
+		// Clear the text field
 		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtname.setText("");
+				txtedition.setText("");
+				txtprice.setText("");
+				txtbid.setText("");
+				txtname.requestFocus();
+			}
+		});
 		btnClear.setFont(new Font("Gulim", Font.PLAIN, 13));
 		btnClear.setBounds(343, 222, 114, 60);
 		panel.add(btnClear);
@@ -191,6 +209,37 @@ public class Crud {
 		panel_1.add(lblNewLabel_1_2);
 		
 		txtbid = new JTextField();
+		txtbid.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				try {
+					String id = txtbid.getText();
+					
+					// Prepare Statement
+					pst = con.prepareStatement("Select name, edition, price from books where id = ?");
+					// Set parameter values for type and position
+					pst.setString(1, id);
+					// Execute the query
+					ResultSet rs = pst.executeQuery();
+					
+					if(rs.next() == true) {
+						String name = rs.getString(1);
+						String edition = rs.getString(2);
+						String price = rs.getString(3);
+						
+						txtname.setText(name);
+						txtedition.setText(edition);
+						txtprice.setText(price);
+					} else {
+						txtname.setText("");
+						txtedition.setText("");
+						txtprice.setText("");
+					}
+				} catch (SQLException ex) {
+					
+				}
+			}
+		});
 		txtbid.setColumns(10);
 		txtbid.setBounds(146, 24, 271, 26);
 		panel_1.add(txtbid);
@@ -203,11 +252,68 @@ public class Crud {
 		scrollPane.setViewportView(table);
 		
 		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String bname, edition, price, bid;
+				
+				bname = txtname.getText();
+				edition = txtedition.getText();
+				price = txtprice.getText();
+				bid = txtbid.getText();
+				
+				try {
+					pst = con.prepareStatement("Update books Set name = ?, edition = ?, price = ? Where id = ?");
+					pst.setString(1, bname);
+					pst.setString(2, edition);
+					pst.setString(3, price);
+					pst.setString(4, bid);
+					pst.executeUpdate();
+		
+					JOptionPane.showMessageDialog(null, "Record Updated");
+					table_load();
+					txtname.setText("");
+					txtedition.setText("");
+					txtprice.setText("");
+					txtbid.setText("");
+					txtname.requestFocus();
+					
+				}
+				catch(SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnUpdate.setFont(new Font("Gulim", Font.PLAIN, 13));
 		btnUpdate.setBounds(688, 440, 114, 60);
 		frame.getContentPane().add(btnUpdate);
 		
 		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String bid;
+				
+				bid = txtbid.getText();
+				
+				try {
+					pst = con.prepareStatement("Delete from books Where id = ?");
+					pst.setString(1, bid);
+					pst.executeUpdate();
+		
+					JOptionPane.showMessageDialog(null, "Record Deleted");
+					table_load();
+					txtname.setText("");
+					txtedition.setText("");
+					txtprice.setText("");
+					txtbid.setText("");
+					txtname.requestFocus();
+					
+				}
+				catch(SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnDelete.setFont(new Font("Gulim", Font.PLAIN, 13));
 		btnDelete.setBounds(814, 440, 114, 60);
 		frame.getContentPane().add(btnDelete);
